cc47ed13d7c269c3cf7ee1b913b534ba51ddace1
BrittDiepenhorst <65465779+BrittDiepenhorst@users.noreply.github.com>
2021-01-29 18:44:55 +0100

    Add files via upload

---
+++ Module 3/M03L13-03/script.js
@@ -0,0 +1,34 @@
+const Age = 18;
+
+if (Age > 18) {
+
+    console.log("Je bent ouder dan 18 jaar, je mag naar binnen.")
+
+} else if (Age == 18) {
+
+    console.log("Je bent 18 jaar, je  mag naar  binnen.")
+
+} else {
+
+    console.log("Je bent jonger dan 18 jaar, je mag niet naar binnen en je wordt vriendelijke verzocht buiten te blijven.")
+}
+
+const isFemale = false;
+
+if (isFemale) {
+
+    console.log("Ladies Night!")
+} else {
+
+    console.log("Het is een Ladies Night, je bent helaas niet welkom.")
+}
+
+const driverStatus = "bob";
+
+if (driverStatus == "bob") {
+
+    console.log("Je mag gaan rijden.")
+} else {
+
+    console.log("Je mag niet rijden.")
+}
