311f6d29a25f557be67b509c032e11b5344dcf73
BrittDiepenhorst <65465779+BrittDiepenhorst@users.noreply.github.com>
2021-02-05 14:57:31 +0100

    Add files via upload

---
+++ Module 3/M03L14-07/script.js
@@ -0,0 +1,22 @@
+const makePizza = function () {
+    console.log("Make the pizza dough");
+    console.log("Roll out the pizza dough");
+    console.log("Make the tomatosaus");
+    console.log("Put the tomatosaus on the pizza dough");
+    console.log("Put some vegetables on the pizza and on top of that some cheese");
+    console.log("Place the pizza in the oven");
+};
+
+makePizza();
+makePizza();
+
+const makeSushi = function () {
+    console.log("Get some rice");
+    console.log("Put some cucumber and avocado on it");
+    console.log("Roll it all up");
+    console.log("Cut it  into pieces");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
